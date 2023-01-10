ed2efdab0cf75d9134f088bcc645cc4c055c7b4d
Matt Dupree <kmdupr33@gmail.com>
2023-01-10 15:12:11 -0500

    containerized notebook execution and uploading successful

--- verus/_modidx.py
+++ verus/_modidx.py
@@ -5,4 +5,12 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://getdatachimp.github.io',
                 'git_url': 'https://github.com/getdatachimp/verus',
                 'lib_path': 'verus'},
-  'syms': {'verus.core': {'verus.core.foo': ('core.html#foo', 'verus/core.py')}}}
+  'syms': { 'verus.core': { 'verus.core.Task': ('core.html#task', 'verus/core.py'),
+                            'verus.core._execute': ('core.html#_execute', 'verus/core.py'),
+                            'verus.core._get_automations': ('core.html#_get_automations', 'verus/core.py'),
+                            'verus.core._get_source': ('core.html#_get_source', 'verus/core.py'),
+                            'verus.core._input_value_to_arg_value': ('core.html#_input_value_to_arg_value', 'verus/core.py'),
+                            'verus.core._run_every': ('core.html#_run_every', 'verus/core.py'),
+                            'verus.core._task_to_script': ('core.html#_task_to_script', 'verus/core.py'),
+                            'verus.core._update_task_status': ('core.html#_update_task_status', 'verus/core.py'),
+                            'verus.core.activate': ('core.html#activate', 'verus/core.py')}}}
